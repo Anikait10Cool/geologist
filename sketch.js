75fc3bde9156b5a97c8cf213e813c9b2766ddc0a
Anikait10Cool <77854407+Anikait10Cool@users.noreply.github.com>
2021-02-26 13:50:00 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -17,7 +17,7 @@ function setup() {
 
 function draw() {
   
-  background(0);
+  background(255);
   Engine.update(engine);
     console.log(hammer.body.position.x);
     console.log(hammer.body.position.y);
